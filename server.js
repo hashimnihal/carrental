cdc9083dee11889d03c9d078e076768b6acdd5e8
MohammadHafeez1 <mohammadhafeezzz01@gmail.com>
2025-03-20 16:12:49 +0530

    backend done

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.use(cookieParser());
 
 app.use(
     cors({
-        origin: 'http://localhost:3000', // ✅ Your frontend's origin
+        origin: ['http://localhost:3000', 'https://carrental-smoky.vercel.app'], // ✅ Your frontend's origin
         credentials: true, // ✅ Allow sending cookies
     })
 );
